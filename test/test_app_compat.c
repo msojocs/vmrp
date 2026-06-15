d2e20edee7e4b596171cad6f3b0ce3bd0ba08001
msojocs <msojocs@users.noreply.github.com>
2026-06-15 16:56:20 +0800

    perf: 使用内部名判断兼容代码

--- test/test_app_compat.c
+++ test/test_app_compat.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "../src/include/app_compat.h"
 
@@ -21,6 +22,30 @@ const AppCompatProfile app_compat_gxdzc = {
     .name = "gxdzc.mrp",
 };
 
+static int write_test_mrp_header(const char *path, const char *internal_name) {
+    unsigned char header[240];
+    FILE *fp;
+    size_t name_len;
+
+    memset(header, 0, sizeof(header));
+    memcpy(header, "MRPG", 4);
+    header[4] = (unsigned char)sizeof(header);
+    header[8] = (unsigned char)sizeof(header);
+    name_len = strlen(internal_name);
+    if (name_len > 12)
+        name_len = 12;
+    memcpy(header + 16, internal_name, name_len);
+
+    fp = fopen(path, "wb");
+    if (!fp)
+        return -1;
+    if (fwrite(header, 1, sizeof(header), fp) != sizeof(header)) {
+        fclose(fp);
+        return -1;
+    }
+    return fclose(fp) == 0 ? 0 : -1;
+}
+
 int test_app_compat_run_all(void) {
     int failures = 0;
 
@@ -36,13 +61,54 @@ int test_app_compat_run_all(void) {
                    "D:\\Github\\vmrp\\build\\Debug\\mythroad\\gghjt.mrp",
                    &app_compat_gghjt);
 
+    printf("[TEST] app compat profile selection accepts id-prefixed filenames\n");
+    ASSERT_PROFILE("prefixed gghjt filename", "018982845369-gghjt.mrp",
+                   &app_compat_gghjt);
+
+    printf("[TEST] app compat profile selection accepts id-prefixed Windows paths\n");
+    ASSERT_PROFILE("prefixed Windows gghjt path",
+                   "C:\\Users\\chenqiancheng\\Documents/mythroad\\018982845369-gghjt.mrp",
+                   &app_compat_gghjt);
+
+    printf("[TEST] app compat profile selection accepts renamed MRP header names\n");
+    {
+        const char *path = "test_vmrp_app_compat_renamed.mrp";
+        if (write_test_mrp_header(path, "gghjt.mrp") != 0) {
+            printf("  FAIL: unable to create test MRP header: %s\n", path);
+            failures++;
+        } else {
+            ASSERT_PROFILE("renamed MRP with gghjt header", path,
+                           &app_compat_gghjt);
+        }
+        remove(path);
+    }
+
+    printf("[TEST] app compat profile selection ignores unknown MRP header names\n");
+    {
+        const char *path = "test_vmrp_app_compat_unknown.mrp";
+        if (write_test_mrp_header(path, "unknown.mrp") != 0) {
+            printf("  FAIL: unable to create test MRP header: %s\n", path);
+            failures++;
+        } else {
+            ASSERT_PROFILE("renamed MRP with unknown header", path, NULL);
+        }
+        remove(path);
+    }
+
     printf("[TEST] app compat profile selection accepts mixed separators\n");
     ASSERT_PROFILE("mixed separator gxdzc path",
                    "D:\\Github\\vmrp/build\\Debug/mythroad\\gxdzc.mrp",
                    &app_compat_gxdzc);
 
+    printf("[TEST] app compat profile selection requires prefix separator\n");
+    ASSERT_PROFILE("suffix without prefix separator",
+                   "D:\\Github\\vmrp\\build\\Debug\\mythroad\\018982845369gghjt.mrp",
+                   NULL);
+
     printf("[TEST] app compat profile selection ignores unknown apps\n");
     ASSERT_PROFILE("unknown app path", "D:\\Github\\vmrp\\foo.mrp", NULL);
+    ASSERT_PROFILE("unknown prefixed app path",
+                   "D:\\Github\\vmrp\\018982845369-foo.mrp", NULL);
 
     return failures;
 }
